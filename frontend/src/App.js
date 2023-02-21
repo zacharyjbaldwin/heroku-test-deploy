67c7473d9892562275e7643d5b0db594570ce90e
Zachary Baldwin <zacharyjbaldwin@gmail.com>
2023-02-21 16:34:22 -0600

    Created login page view. It is not yet hooked up to the backend.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,28 @@
-import { useState, useCallback } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import './App.css';
 import ToolBar from './components/Navigation/Toolbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Profile from './pages/Profile';
 import { AuthContext } from './shared/context/auth-context';
+import { useAuth } from './shared/hooks/auth-hook';
 
 function App() {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userId, setUserId] = useState(false);
-
-  const login = useCallback(userId => {
-    setIsLoggedIn(true);
-    setUserId(userId);
-  }, []);
-
-  const logout = useCallback(() => {
-    setIsLoggedIn(false);
-    setUserId(null);
-  }, []);
+  const { token, login, logout, userId } = useAuth();
 
   let routes;
-  if (isLoggedIn) {
+  if (token) {
     routes = (
       <Switch>
-        <Route path="/profile">
-          <Login />
+        <Route path="/" exact>
+          <Home />
         </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Redirect to="/" />
       </Switch>
     );
   } else {
@@ -47,7 +41,8 @@ function App() {
 
   return (
     <AuthContext.Provider value={{
-      isLoggedIn: isLoggedIn,
+      isLoggedIn: !!token,
+      token: token,
       userId: userId,
       login: login,
       logout: logout
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,96 @@
+import { useReducer } from "react";
+
+const EMAIL_REGEX = /^[A-Za-z0-9\.]+@[A-Za-z0-9\.]+$/; // eslint-disable-line
+
+const formReducer = (state, action) => {
+    switch (action.type) {
+        case 'EMAIL_INPUT':
+            return {
+                ...state,
+                email: {
+                    value: action.value,
+                    isValid: action.value.match(EMAIL_REGEX) !== null,
+                    touched: true
+                },
+                isValid: action.value.match(EMAIL_REGEX) !== null && state.password.isValid
+            }
+        case 'EMAIL_BLUR':
+            return {
+                ...state,
+                email: { ...state.email, touched: true }
+            }
+        case 'PASSWORD_INPUT':
+            return {
+                ...state,
+                password: {
+                    value: action.value,
+                    isValid: action.value.length >= 6,
+                    touched: true
+                },
+                isValid: state.email.isValid && action.value.length >= 6
+            }
+        case 'PASSWORD_BLUR':
+            return {
+                ...state,
+                password: { ...state.password, touched: true }
+            }
+        default: return state;
+    }
+};
+
 const Login = props => {
-    return <h2>Login page</h2>;
+
+    const [formState, dispatch] = useReducer(formReducer, {
+        email: {
+            value: '',
+            isValid: false,
+            touched: false
+        },
+        password: {
+            value: '',
+            isValid: false,
+            touched: false
+        },
+        isValid: false
+    });
+
+    const onSubmit = e => {
+        e.preventDefault();
+        alert('501 not implemented')
+    }
+
+    return (
+        <div className="col-md-4 offset-md-4">
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">Login</h5>
+                    <form onSubmit={onSubmit}>
+                        <div className="form-group mb-3">
+                            <label className="form-label">Email Address</label>
+                            <input type="email"
+                                id="email"
+                                className={`form-control ${!formState.email.isValid && formState.email.touched ? 'is-invalid' : ''}`}
+                                onChange={e => { dispatch({ type: 'EMAIL_INPUT', value: e.target.value }) }}
+                                onBlur={() => { dispatch({ type: 'EMAIL_BLUR' }) }}
+                                ></input>
+                            {!formState.email.isValid && formState.email.touched && <small className="invalid-feedback form-text">Email address is required.</small>}
+                        </div>
+                        <div className="form-group mb-3">
+                            <label className="form-label">Password</label>
+                            <input type="password"
+                                id="password"
+                                className={`form-control ${!formState.password.isValid && formState.password.touched ? 'is-invalid' : ''}`}
+                                onChange={e => { dispatch({ type: 'PASSWORD_INPUT', value: e.target.value }) }}
+                                onBlur={() => { dispatch({ type: 'PASSWORD_BLUR' }) }}
+                                ></input>
+                            {!formState.password.isValid && formState.password.touched && <small className="invalid-feedback form-text">Password must be at least 6 characters in length.</small>}
+                        </div>
+                        <button disabled={!formState.isValid} type="submit" className="btn btn-success col-12">Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
