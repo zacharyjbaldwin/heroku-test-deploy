e55b30f7762930e6bf13874fc37eeffc873e79a3
Zachary Baldwin <zacharyjbaldwin@gmail.com>
2023-02-23 16:15:02 -0600

    added more routes for managing tutors: one for creating tutors, one for updating tutors, and one for deleting tutors

--- api/controllers/tutors.controller.js
+++ api/controllers/tutors.controller.js
@@ -1,7 +1,8 @@
+const { validationResult } = require('express-validator');
 const User = require('../models/user.model');
 
 module.exports.getTutors = (req, res) => {
-    const pageSize = req.query.pageSize || 10;
+    const pageSize = req.query.pageSize || 8;
     const pageNumber = req.query.pageNumber || 0;
     const filter = { isTutor: true };
     const search = req.query.searchQuery ? req.query.searchQuery.trim().toLowerCase() : undefined;
@@ -41,3 +42,39 @@ module.exports.getTutorById = (req, res) => {
         })
         .catch(() => { res.status(500).json({ message: 'Failed to fetch user.' }); });
 };
+
+// do not use this route for signing up tutors
+module.exports.createTutor = (req, res) => {
+    if (!validationResult(req).isEmpty()) {
+        return res.status(400).json({ message: 'One or more required parameters is missing or malformed.' });
+    }
+
+    const { firstName, lastName, email, aboutMe, skills, availability } = req.body;
+
+    const tutor = new User({
+        firstName,
+        lastName,
+        email, 
+        isTutor: true,
+        aboutMe,
+        skills,
+        availability,
+        password: 'password123'
+    });
+
+    tutor.save()
+        .then(tutor => { res.status(201).json(tutor); })
+        .catch(() => { res.status(500).json({ message: 'Failed to create tutor.' }); });
+};
+
+module.exports.updateTutor = (req, res) => {
+    User.findByIdAndUpdate(req.params.tutorId, req.body, { new: true })
+        .then(tutor => { res.status(200).json({ message: 'Updated tutor', tutor }); })
+        .catch(() => { res.status(500).json({ message: 'Failed to update tutor.' }); });
+};
+
+module.exports.deleteTutor = (req, res) => {
+    User.findByIdAndDelete(req.params.tutorId)
+        .then(() => { res.status(204).json({ message: 'Deleted tutor.' }); })
+        .catch(() => { res.status(500).json({ message: 'Failed to delete tutor.' }); });
+}
--- api/routes/tutors.routes.js
+++ api/routes/tutors.routes.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
 const controller = require('../controllers/tutors.controller');
+const { check } = require('express-validator');
 
 router.get('/', controller.getTutors);
+
 router.get('/:tutorId', controller.getTutorById);
+
+router.post('/', 
+    [
+        check('firstName').notEmpty(),
+        check('lastName').notEmpty(),
+        check('email').normalizeEmail().isEmail(),
+        check('aboutMe').notEmpty(),
+        check('skills').notEmpty(),
+        check('availability').notEmpty()
+    ],
+    controller.createTutor);
+
+router.patch('/:tutorId', controller.updateTutor);
+
+router.delete('/:tutorId', controller.deleteTutor);
 
 module.exports = router;
