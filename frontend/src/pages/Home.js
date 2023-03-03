6b7d60af9d72d9ebd25ab7a8b57ac9274f579efe
aaron <aaron.c.xu92@gmail.com>
2023-03-03 15:58:50 -0600

    add some css styles and created home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,23 @@
+import React from 'react';
+import '../App.css';
+import image from "../images/utd_background.jpg";
+
 const Home = props => {
-    return <h2>Home page</h2>;
-};
+    return(
+        <div className = "Home" style={{ backgroundImage: `url(${image})` }}>
+            <div className = "box">
+                <h1 align = "left">UTD Tutoring</h1>
+                <h2 align = "left">For UTD Students by UTD Students</h2>
+                <a href="/Signup">
+                    <button className="b">Register</button>
+                </a>
+                <a href="/Login">
+                    <button className="b">Log In</button>
+                </a>
+                <button className="b">Learn More</button>
+            </div>
+        </div>
+    )
+}
 
 export default Home;
